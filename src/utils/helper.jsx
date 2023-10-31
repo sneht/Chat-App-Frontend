1f2ec799f3f79d173a0d62a55bc0f3fe463dc70b
sneht <sneh.frontendarmy@gmail.com>
2023-10-31 18:14:54 +0530

    [Live URL]

--- src/utils/helper.jsx
+++ src/utils/helper.jsx
@@ -1,3 +1,1 @@
-export const ENDPOINT_URL =
-  // "https://chat-app-backend-qtdx.onrender.com/";
-  "http://localhost:4000/";
+export const ENDPOINT_URL = "https://chat-app-backend-qtdx.onrender.com/";
